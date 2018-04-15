d47677bec399ee142b68fb6a9171fcce1215c500
karolinneczka <okaa93@gmail.com>
2018-04-15 23:45:15 +0200

    Add basic files and add a simple program in js file to calculate field of triangle

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+alert('Hello! I will calculate a field of your triangle, ok? ');
+
+var a = prompt('Give the base of the triangle'); 
+var h = prompt('Give the height of the triangle');
+var triangleArea=a*h/2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
